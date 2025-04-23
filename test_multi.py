c0b06f103b3483318387b1373beb918d2773ee22
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-23 17:07:05 +0300

    adding multiplication files

---
+++ test_multi.py
@@ -0,0 +1,10 @@
+def test_multiplication():
+
+    assert multiplication(1, 2) == 2
+
+    assert multiplication(20, 10) == 200
+
+    assert multiplication(-1, 2) == -2
+
+def multiplication(a,b):
+    return a * b
---
+++ test_sub.py
@@ -0,0 +1,10 @@
+def test_substract():
+
+    assert substract(4, 2) == 2
+
+    assert substract(20, 10) == 10
+
+    assert substract(0, 0) == 0
+
+def substract(a,b):
+    return a - b
